a607417c653184fc9f6ac04c4eecce5114ec8802
MICROHEM <MICROHEM@REZHWAN-EXPERT>
2022-03-17 21:07:11 +0300

    Add project files.

---
+++ ReBulkMail/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Re Bulk Mail Sender")]
+[assembly: AssemblyDescription("C# bulk mail sender")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("By Rezhwan Ameer")]
+[assembly: AssemblyProduct("Re Bulk Mail Sender")]
+[assembly: AssemblyCopyright("Copyright ©  Rezhwan Ameer  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("91073da4-7d94-465b-b977-3e45c1401430")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
